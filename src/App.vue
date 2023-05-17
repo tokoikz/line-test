c90fb5a11e2a960e3dd643cdd66520dfd78c6b7b
Kazunori Tokoi <kazunori.tokoi@regrit-p.com>
2023-05-17 15:18:42 +0900

    axiosをinstall

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- UI components here -->
+  <button @click="sendRichMessage">Send Message</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  methods: {
+    sendRichMessage() {
+      const headers = {
+        "Authorization": "Bearer {CHANNEL_ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+      };
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      const data = {
+        "to": "{USER_ID}",
+        "messages": [
+          {
+            "type": "template",
+            "altText": "This is a buttons template",
+            "template": {
+              "type": "buttons",
+              "thumbnailImageUrl": "https://example.com/original.jpg",
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "Menu",
+              "text": "Please select",
+              "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
+              },
+              "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=123"
+                },
+                {
+                  "type": "postback",
+                  "label": "Add to cart",
+                  "data": "action=add&itemid=123"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/123"
+                }
+              ]
+            }
+          }
+        ]
+      };
+
+      axios
+        .post('https://api.line.me/v2/bot/message/push', data, { headers })
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
